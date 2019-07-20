9179f5ea30ae80850bec3ce9203297191ad82c78
Demetrius Rowland <demetriushrowland@gmail.com>
2019-07-19 20:18:24 -0500

    Computes log likelihood and maximizes.

--- successor_representation/util.py
+++ successor_representation/util.py
@@ -1,4 +1,5 @@
 import pandas
+import numpy as np
 
 
 # receives a tab-separated input file and returns the data frame containing its contents
@@ -12,3 +13,12 @@ def get_objects(directory):
     matrix = read_file (directory)
     obj_sequence = matrix[[' objnum']]    
     return obj_sequence
+
+def get_induction_data (directory):
+    matrix = read_file (directory)
+    cue_sequence = np.array(matrix[[' CueNum']])
+    opt1_sequence = np.array(matrix[[' Opt1Num']])
+    opt2_sequence = np.array(matrix[[' Opt2Num']])
+    response_sequence = np.array(matrix[[' Resp']])
+    return cue_sequence, opt1_sequence, opt2_sequence, response_sequence
+    
