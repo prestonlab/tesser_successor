97e4b26931ddfd58a8cf15e725d92f6835c5afc5
Demetrius Rowland <demetriushrowland@gmail.com>
2019-07-30 13:21:53 -0500

    Adds fit and sr

---
+++ tesser/fit.py
@@ -0,0 +1,31 @@
+import pandas
+import csv
+import matplotlib.pyplot as plt
+import learn
+import util
+import numpy as np
+import numpy.linalg as la
+import scipy as sp
+import scipy.spatial.distance as dist
+import sr
+
+def maximize_likelihood (obj_sequence_directory, induction_directory):
+    h = 10e-3
+    alpha_max, gamma_max = 0.0, 0.0
+    alpha, gamma = h, h
+    log_likelihood_max = get_log_likelihood (alpha, gamma, obj_sequence_directory, induction_directory)
+    log_likelihoods = []
+    
+    
+    while alpha <= 1:
+        gamma = h
+        while gamma <= 1:
+            log_likelihood = get_log_likelihood(alpha, gamma, obj_sequence_directory, induction_directory)
+            log_likelihoods.append (log_likelihood)
+            if log_likelihood > log_likelihood_max:
+                log_likelihood_max = log_likelihood
+                alpha_max, gamma_max = alpha, gamma
+            gamma += h
+        alpha += h
+    
+    return alpha_max, gamma_max
