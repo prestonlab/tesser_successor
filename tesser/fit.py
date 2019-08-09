0660a12dcdbb80e24aa384737ab7c92dabefb7a0
demetriushrowland <40215063+demetriushrowland@users.noreply.github.com>
2019-08-09 16:06:57 -0500

    Update fit.py

--- tesser/fit.py
+++ tesser/fit.py
@@ -1,6 +1,38 @@
 import numpy as np
 import sr
 
+def pBGivenA (A, B, C, SR):
+    return SR[A][B] / (SR[A][B] + SR[A][C])
+
+def pCGivenA (A, B, C, SR):
+    return 1 - pBGivenA (A, B, C, SR)
+
+def likelihood (cue, opt1, opt2, response, SR):
+    if response == 0:
+        return pBGivenA (cue, opt1, opt2, SR)
+    if response == 1:
+        return pCGivenA (cue, opt1, opt2, SR)
+    
+
+def get_log_likelihood(SUBJECT, GAMMA, ALPHA):
+    ''' This function gives the probability of obtaining the choices in the run, given
+    specific values for alpha, gamma.
+    '''
+    SR = sr.explore_runs ('once', SUBJECT, GAMMA, ALPHA)
+    data, keys = util.read_files(SUBJECT, 'induction')
+    cue_sequence, opt1_sequence, opt2_sequence, response_sequence = util.get_induction_data (data[keys[0]])
+    num_trials = len (cue_sequence)
+    log_likelihood = 0
+    for trial_num in range (num_trials):
+        try:
+            cue_num, opt1_num, opt2_num, response_num = int(cue_sequence[trial_num]) - 1, int(opt1_sequence[trial_num]) - 1, int(opt2_sequence[trial_num]) - 1, int(response_sequence[trial_num]) - 1
+            trial_probability = likelihood (cue_num, opt1_num, opt2_num, response_num, SR)
+            log_likelihood += np.log (trial_probability)
+        except ValueError:
+            pass
+    
+    return log_likelihood
+
 def maximize_likelihood (subject):
     h = 10e-3
     alpha_max, gamma_max = 0.0, 0.0
