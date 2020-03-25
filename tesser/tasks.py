b53acad9703ae5bf174dc15d539bce607075a539
Athula Pudhiyidath <athula@apmac7.clm.utexas.edu>
2020-03-24 23:22:41 -0500

    cleaning and testing functions

--- tesser/tasks.py
+++ tesser/tasks.py
@@ -14,11 +14,13 @@ This function is to evaluate performance in the temporal structure learning, gro
 
 import pandas as pd
 import numpy as np
-import scipy.spatial.distance as sd
+import scipy.spatial.distance as sd 
+from scipy.spatial.distance import pdist, squareform
 import matplotlib
 import matplotlib.pyplot as plt
 from . import util
 from . import network
+
 
 ###########################
 # inductive inference task
@@ -51,164 +53,186 @@ def induct_avg_all(data_dir):
         this_avg = induct_avg(this_induct)
         part_avg_list.append(this_avg)
     # convert list to df
-    part_avg_df = pd.DataFrame(part_avg_list, columns=['Participant', 'Overall', 'Prim', 'Bound1', 'Bound1'])
+    part_avg_df = pd.DataFrame(part_avg_list, columns=['participant', 'overall', 'prim', 'bound1', 'bound2'])
     return part_avg_df
 
 
-def induct_avg_split_high(participant_induct_avg_df):
+def induct_avg_split_high(all_participant_induct_avg_df):
     """High performers based on a median split of induction."""
 
-    induct_average = participant_induct_avg_df["Overall"].mean()
+    induct_average = all_participant_induct_avg_df["overall"].mean()
 
-    induct_above = participant_induct_avg_df[participant_induct_avg_df["Overall"] > induct_average]
+    induct_above = all_participant_induct_avg_df[all_participant_induct_avg_df["overall"] > induct_average]
     return induct_above
 
 
-def induct_avg_split_low(participant_induct_avg_df):
+def induct_avg_split_low(all_participant_induct_avg_df):
     """Low performers based on a median split of induction."""
 
-    induct_average = participant_induct_avg_df["Overall"].mean()
+    induct_average = all_participant_induct_avg_df["overall"].mean()
 
-    induct_below = participant_induct_avg_df[participant_induct_avg_df["Overall"] < induct_average]
+    induct_below = all_participant_induct_avg_df[all_participant_induct_avg_df["overall"] < induct_average]
     return induct_below
 
+
+def induct_bias(induct_df):
+    """Get average induction task bias by question type."""
+    full_len = len(induct_df)
+    #primary
+    induct_prim = induct_df[induct_df["QuestType"]=="Prim"]
+    prim_len = len(induct_prim)
+    #bound1
+    induct_bound1 = induct_df[induct_df["QuestType"]=="Bound1"]
+    bound1_len = len(induct_bound1)
+    #bound2
+    induct_bound2 = induct_df[induct_df["QuestType"]=="Bound2"]
+    bound2_len = len(induct_bound2)
+
+    #get overall mean
+    overall_acc_total = len(induct_df[induct_df["Acc"]==1])
+    overall_acc_prop = overall_acc_total/full_len
+    overall_mis_total = len(induct_df[induct_df["Acc"]==0])
+    overall_mis_prop = overall_mis_total/full_len
+
+    overall_bias = overall_acc_prop - overall_mis_prop
+    
+    #get primary mean (acc + RT)
+    prim_acc_total = len(induct_prim[induct_prim["Acc"]==1])
+    prim_acc_prop = prim_acc_total/prim_len
+    prim_mis_total = len(induct_prim[induct_prim["Acc"]==0])
+    prim_mis_prop = prim_mis_total/prim_len
+
+    prim_bias = prim_acc_prop - prim_mis_prop
+
+    #get bound1 mean (acc + RT)
+    bound1_acc_total = len(induct_bound1[induct_bound1["Acc"]==1])
+    bound1_acc_prop = bound1_acc_total/bound1_len
+    bound1_mis_total = len(induct_bound1[induct_bound1["Acc"]==0])
+    bound1_mis_prop = bound1_mis_total/bound1_len
+
+    bound1_bias = bound1_acc_prop - bound1_mis_prop  
+    
+    #get bound2 mean (acc + RT)
+    bound2_acc_total = len(induct_bound2[induct_bound2["Acc"]==1])
+    bound2_acc_prop = bound2_acc_total/bound2_len
+    bound2_mis_total = len(induct_bound2[induct_bound2["Acc"]==0])
+    bound2_mis_prop = bound2_mis_total/bound2_len
+
+    bound2_bias = bound2_acc_prop - bound2_mis_prop
+    
+    return overall_bias, prim_bias, bound1_bias, bound2_bias
+
+
+def induct_bias_all(data_dir):
+    """Average induction task bias for all participants."""
+
+    # want to load a list of the participants and loop through
+    part_bias_list = []
+    part_list = util.subj_list()
+    for i in range(len(part_list)):
+        part_num = part_list[i]
+        this_induct = util.load_induct_df_all(data_dir, part_num)
+        overall_bias, prim_bias, bound1_bias, bound2_bias = induct_bias(this_induct)
+        part_bias = [part_num, overall_bias, prim_bias, bound1_bias, bound2_bias]        
+        part_bias_list.append(part_bias)
+    # convert list to df
+    part_bias_df = pd.DataFrame(part_bias_list, columns=['participant', 'overall', 'prim', 'bound1', 'bound2'])
+    return part_bias_df
 
 ###########################
 # grouping task
 ###########################
 
-def group_dist_mat(group_mat):
-    """Calculate pairwise object distances from a grouping matrix."""
-
-    rows, cols = np.nonzero(group_mat)
-
-    # getting the identity of the objects from their coordinates:
-    objects = group_mat[rows, cols]
-
-    # stacking and transposing the coordinates:
-    # vertically stacking horizontal list of rows, columns, such that row1 = rows, row2 = cols
-    coord_stack = np.vstack((rows, cols))
-    # then transposing them to two columns, such that col1 = rows, col2 = cols
-    coord_list = coord_stack.T
-    # recall that the objects list is in the order in which they were interpreted
-    # to understand at what index the lowest (1) and highest (21) objects are in, can use np.argsort
-    # this gives you the index of each of those objects from low to high as in the objects + coord list
-    sort_ind = np.argsort(objects)
-
-    # now can use the coord list and sort by the low to high sort_ind of object index
-    # so now the list is the coordinates of objects 1-21 in that order
-    coord_list_sort = coord_list[sort_ind]
-
-    # now we can take coord_list_sort (i.e. coordinates of objects in object order 1-21) and make a distance matrix
-    group_dist_matrix = sd.squareform(sd.pdist(coord_list_sort, 'Euclidean'))
-    return group_dist_matrix
+def euc_dist_mat(data_dir, subject):
+    """Calculate pairwise object distances from a grouping array output."""
+    group_load = util.load_group(data_dir, subject)
+    df_group = pd.DataFrame(group_load)
+    df_group.columns = np.arange(len(df_group.columns))
+    
+    all_num = []
+    all_row = []
+    all_col = []
+    for n in range(1, 22, 1):
+        this_row_col = np.where(df_group == n) #row [0], column [1]
+        this_row = np.int(this_row_col[0])
+        this_col = np.int(this_row_col[1])
+        all_num.append(n)
+        all_row.append(this_row)
+        all_col.append(this_col)
+  
+    all_num_row_col = pd.DataFrame(list(zip(all_num, all_row, all_col)), columns =['node', 'row', 'col'])
+    all_row_col = pd.DataFrame(list(zip(all_row, all_col)), columns =['row', 'col'])
+    all_row_col_array = all_row_col.values
+    
+    dist_bw_sq = squareform(pdist(all_row_col_array, "euclidean"))
+    return dist_bw_sq
 
 
-def group_dist_df(group_dist_matrix):
-    """Grouping task distance matrix as a data frame."""
+def euc_dist_comm_avg(data_dir, subject):
+    # load structure learning and induction data for one subject
+    dist_bw_sq = euc_dist_mat(data_dir, subject)
 
-    # first convert to a dataframe
-    group_dist_dataframe = pd.DataFrame(group_dist_matrix)
+    #get only the upper half, excluding diagonal (b/c the same above and below diagonal)
+    dist_upper = np.triu(dist_bw_sq)
 
-    # reindex the data rows
-    group_dist_dataframe.index = group_dist_dataframe.index + 1
+    #add in NaNs next
+    dist_upper[np.tril_indices(dist_upper.shape[0], 0)] = np.nan
 
-    # reindx the data cols
-    group_dist_dataframe.columns = group_dist_dataframe.columns + 1
+    comm_struct = network.temp_node_info()
 
-    return group_dist_df  # now index rows, cols = 1:21
+    #now create an adjacency matrix basd on the relationships b/w items
+    #can use this adjacency matrix to create a mask to cover the distance above 
+    comm = network.community_mat(comm_struct)
+    
+    #get only the upper half, excluding diagonal (b/c the same above and below diagonal)
+    comm_upper = np.triu(comm)
 
+    #add in 7 for the diagonal that does not matter, b/c rest will be using 1 and 0 for (within and across) 
+    comm_upper[np.tril_indices(comm_upper.shape[0], 0)] = .01
 
-def obj_combo(list_objs):
-    """Make list of unique object combinations."""
+    #now to calculate the distances whose adj_upper == 1 (within) and adj_upper == 0 (across)
+    #within
+    comm_within_dist = np.ma.masked_where(comm_upper == 0, dist_upper)
 
-    # have now gotten all the ones in community 1, reindex the data rows:
-    list_objs = list_objs.reset_index()
-    pair_list = []
-    for x in range(len(list_objs)):
+    #now to take the mean of the distances
+    within_avg_dist_comm = np.nanmean(comm_within_dist)
 
-        if x == len(list_objs):
-            break
-        else:
-            for q in range(x, len(list_objs) - 1):
-                y = q + 1
-                pair = [list_objs['node'][x], list_objs['node'][y]]
-                pair_list.append(pair)
-    return pair_list
+    #across
+    comm_across_dist = np.ma.masked_where(comm_upper == 1, dist_upper)
 
+    #now to take the mean of the distances
+    across_avg_dist_comm = np.nanmean(comm_across_dist)
 
-def specific_within_comm_dist(group_dist_m, pair_list):
-    """Within-community distance between objects in a particular community."""
-
-    this_dist_tog = []
-    for l in range(len(pair_list) - 1):
-        this_pair = pair_list[l]
-        mat_x = this_pair[0]
-        mat_y = this_pair[1]
-        # now match to the matrix of similarity
-        dist_df = group_dist_df(group_dist_m)
-        this_dist = dist_df.loc[mat_x, mat_y]
-        this_dist_tog.append(this_dist)
-
-    this_comm_within_dist = np.mean(this_dist_tog)
-    return this_comm_within_dist
+    return within_avg_dist_comm, across_avg_dist_comm
 
 
-def within_comm_dist(group_dist_matrix):
-    """Within-community distance between objects."""
+def euc_dist_comm_avg_all(data_dir):
+    """Average induction task performance for all participants."""
 
-    within_dist_tog = []
-    # getting the structure information
-    comm_all_objs = network.temp_node_info()
-    # print(structure_info)
-    for n in range(1, 4):
-        this_comm_objs = comm_all_objs[comm_all_objs["comm"] == n]
-        this_comm_pairs = obj_combo(this_comm_objs)
-        this_comm_within_dist = specific_within_comm_dist(group_dist_matrix, this_comm_pairs)
-        within_dist_tog.append(this_comm_within_dist)
-    within_comm_d = np.mean(within_dist_tog)
-    return within_comm_d
-
-
-def across_comm_dist(group_dist_matrix):
-    """Across-community distance between objects."""
-
-    # getting the structure information
-    comm_all_objs = network.temp_node_info()
-
-    comm1_objs = comm_all_objs[comm_all_objs["comm"] == 1]
-    comm2_objs = comm_all_objs[comm_all_objs["comm"] == 2]
-    comm3_objs = comm_all_objs[comm_all_objs["comm"] == 3]
-
-    comm1_pairs = obj_combo(comm1_objs)
-    comm2_pairs = obj_combo(comm2_objs)
-    comm3_pairs = obj_combo(comm3_objs)
-
-    within_comm_pairs = comm1_pairs + comm2_pairs + comm3_pairs
-    within_comm_pairs_array = np.array(within_comm_pairs)
-
-    all_comm_pairs = obj_combo(comm_all_objs)
-    all_comm_pairs_array = np.array(all_comm_pairs)
-
-    diff = set(map(tuple, all_comm_pairs_array)) - set(map(tuple, within_comm_pairs_array))
-    diff_list = list(map(list, diff))
-
-    pair_list = diff_list
-    this_dist_tog = []
-    for l in range(len(pair_list) - 1):
-        this_pair = pair_list[l]
-        mat_x = this_pair[0]
-        mat_y = this_pair[1]
-        # now match to the matrix of similarity
-        dist_df = group_dist_df(group_dist_matrix)
-        this_dist = dist_df.loc[mat_x, mat_y]
-        this_dist_tog.append(this_dist)
-
-    across_comm_distance = np.mean(this_dist_tog)
-    return across_comm_distance
+    # want to load a list of the participants and loop through
+    part_euc_avg_list = []
+    part_list = util.subj_list()
+    for i in range(len(part_list)):
+        part_num = part_list[i]
+        within_dist, across_dist = euc_dist_comm_avg(data_dir, part_num)
+        part_avg = [part_num, within_dist, across_dist]
+        part_euc_avg_list.append(part_avg)
+    # convert list to df
+    part_euc_avg_df = pd.DataFrame(part_euc_avg_list, columns=['participant', 'within_dist', 'across_dist'])
+    return part_euc_avg_df
 
 
 def comm_color_map():
+    
+    #community members
+    comm_all_objs = network.temp_node_info()
+    comm1_cent_objs = comm_all_objs[(comm_all_objs["comm"] == 1) & (comm_all_objs["nodetype"] == 0)]
+    comm1_bound_objs = comm_all_objs[(comm_all_objs["comm"] == 1) & (comm_all_objs["nodetype"] == 1)]
+    comm2_cent_objs = comm_all_objs[(comm_all_objs["comm"] == 2) & (comm_all_objs["nodetype"] == 0)]
+    comm2_bound_objs = comm_all_objs[(comm_all_objs["comm"] == 2) & (comm_all_objs["nodetype"] == 1)]
+    comm3_cent_objs = comm_all_objs[(comm_all_objs["comm"] == 3) & (comm_all_objs["nodetype"] == 0)]
+    comm3_bound_objs = comm_all_objs[(comm_all_objs["comm"] == 3) & (comm_all_objs["nodetype"] == 1)]
+
     # Color value constants
     colors = {"d_purple": '#7e1e9c',
               "l_purple": '#bf77f6',
@@ -239,32 +263,33 @@ def comm_color_map():
     return color_list
 
 
-def plot_dist(group_mat_input):
+def plot_dist(group_array):
     color_list = comm_color_map()
     cmap = matplotlib.colors.ListedColormap(color_list)
-    plt.imshow(group_mat_input, cmap=cmap)
+    plt.imshow(group_array, cmap=cmap)
 
 
-def total_group_num(group_mat):
+def total_group_num(group_array):
     # make group_mat into 0 and 1 values instead of object numbers
-    bin_group_mat = (group_mat > 0).astype(int)
+    bin_group_mat = (group_array > 0).astype(int)
     from skimage import measure
     groupmat_labeled = measure.label(bin_group_mat, connectivity=2, return_num=True)
     labeled_total = groupmat_labeled[1] #total number of groups
     return labeled_total
 
 
-def group_clusters(group_mat):
+def group_clusters(group_array):
     # make group_mat into 0 and 1 values instead of object numbers
-    bin_group_mat = (group_mat > 0).astype(int)   
+    bin_group_mat = (group_array > 0).astype(int)   
     from skimage import measure
     groupmat_labeled = measure.label(bin_group_mat, connectivity=2, return_num=True)
+    labeled_total = total_group_num(group_array)
     labeled_array = groupmat_labeled[0] #array of labeled groups
     group_stacked = []
     
     for t in range(1, labeled_total+1):
         this_index = np.where(labeled_array==t)
-        this_group = group_mat[this_index] 
+        this_group = group_array[this_index] 
         this_group_list = this_group.tolist()
         group_stacked.append(this_group_list)
     return group_stacked
