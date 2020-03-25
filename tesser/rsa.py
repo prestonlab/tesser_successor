7aca3b49129221068b903f44a830370df8d96c91
Athula Pudhiyidath <athula@apmac7.clm.utexas.edu>
2020-03-24 19:23:23 -0500

    adding fucntion to read beta images of each roi x trial

--- tesser/rsa.py
+++ tesser/rsa.py
@@ -19,3 +19,35 @@ def multiple_rdm(SR_matrices):
                 pass
     return rdm_matrices
 
+
+def load_betas(data_dir, subject_num, roi): 
+    """ Computes the representational dissimilarity matrix for one matrix. """
+    
+    roi_dir = os.path.join(data_dir, 'item_betas', 'roi')
+
+    # look for directories with the correct pattern
+    file_search = glob(os.path.join(roi_dir + '/' + roi, f'pattern_{subject_num}.txt'))
+    if len(file_search) != 1:
+        raise IOError(f'Problem finding ROI or subject directory for {roi} and {subject_num}')
+    pattern_file = file_search[0]
+    
+    # read log, fixing problem with spaces in column names
+    this_pattern = np.loadtxt(pattern_file)
+
+    null_list = []
+    for run in range(1, 7):
+        this_run_len = run*151
+        null_1 = this_run_len - 151
+        null_list.append(null_1)
+        null_2 = (this_run_len - 151) + 1
+        null_list.append(null_2)
+        null_3 = this_run_len - 2
+        null_list.append(null_3)
+        null_4 = this_run_len - 1
+        null_list.append(null_4)
+        
+    # remove the fixation trials in the runs (these are just filler trials, i.e. the 4 null trials above)
+    this_reformat_pattern = np.delete(this_pattern, null_list, axis=0)
+    
+    #return this_reformat_pattern
+    return this_reformat_pattern
