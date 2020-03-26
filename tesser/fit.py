66c900ef828c7ded6102ac023a5ed9c2966d3010
Athula <athula.pud@gmail.com>
2020-03-26 08:54:07 -0500

    adding comments to functions

--- tesser/fit.py
+++ tesser/fit.py
@@ -115,6 +115,10 @@ def get_induction_prob_correct(struc_df, induc_df, gamma, alpha, tau,
                                use_run=(2, 6)):
     """Get probability of correct response on each induction trial."""
 
+    # this can loop over each subject
+    
+    # use probability induction choice to figure out what the correct response was (not partiicpant response)
+    
     # learn SR based on structure learning data, gamma, and alpha
 
     # iterate over induction test trials
