ef98f62c57a1a98a77e94d74076cb312805a4da3
Athula Pudhiyidath <athula.pud@gmail.com>
2020-05-05 22:26:56 -0500

    Update fit.py

--- tesser/fit.py
+++ tesser/fit.py
@@ -74,7 +74,7 @@ def assess_induct_fit_subject(struct, induct, param):
 
     trial_prob = prob_induct_subject(struct, induct, param['gamma'],
                                      param['alpha'], param['tau'],
-                                     response_key='correct')
+                                     response_key='response')
     induct.loc[:, 'Data'] = induct['Acc']
     induct.loc[:, 'Model'] = trial_prob
     results = induct.melt(id_vars=['SubjNum', 'TrialNum', 'QuestType',
