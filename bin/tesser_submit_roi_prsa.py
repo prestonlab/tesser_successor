13c4c0d1faa9c3cbb8707773a4b131e47b201bab
Neal Morton <mortonne@gmail.com>
2021-02-05 13:35:49 -0600

    feat: support for setting block input

--- bin/tesser_submit_roi_prsa.py
+++ bin/tesser_submit_roi_prsa.py
@@ -7,12 +7,12 @@ import argparse
 from tesser import util
 
 
-def main(subjects, rois, study_dir, res_name):
+def main(subjects, rois, study_dir, res_name, block):
     if subjects is None:
         subjects = util.subj_list()
 
     beh_dir = os.path.join(study_dir, 'batch', 'behav')
-    options = f'--study-dir={study_dir}'
+    options = f'--study-dir={study_dir} -b {block}'
     for roi in rois:
         for subject in subjects:
             print(f'tesser_roi_prsa.py {subject} {beh_dir} {roi} {res_name} {options}')
@@ -22,6 +22,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('rois', help="name of mask to use.")
     parser.add_argument('res_name', help="name of directory to save results.")
+    parser.add_argument('--block', '-b', help='block to include (walk, random)')
     parser.add_argument('--study-dir', help="path to main study data directory.")
     parser.add_argument('--subjects', '-s', help="ID of subjects to process.")
     args = parser.parse_args()
@@ -39,4 +40,4 @@ if __name__ == '__main__':
         inc_subjects = None
 
     inc_rois = args.rois.split(',')
-    main(inc_subjects, inc_rois, env_study_dir, args.res_name)
+    main(inc_subjects, inc_rois, env_study_dir, args.res_name, block=args.block)
